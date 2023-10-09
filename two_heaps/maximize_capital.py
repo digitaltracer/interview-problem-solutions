afe66eb20b4fd8116e7480c4d0131693a3b62ec0
Adarsh N B <digitaltracer@users.noreply.github.com>
2023-10-09 16:18:27 +0530

    fix init spelling; add solution for maximum capital

--- two_heaps/maximize_capital.py
+++ two_heaps/maximize_capital.py
@@ -2,5 +2,55 @@
 
 # Description:
 """
-
+Generate maximum returns with an initial capital c and given portfolios with profits and number of investments k
 """
+
+from typing import List
+from interview_problem_solutions.utils.min_heap import MinHeap
+from interview_problem_solutions.utils.max_heap import MaxHeap
+
+
+def maximum_capital(c: int, k: int, capitals: List[int], profits: List[int]) -> int:
+    current_capital = c
+    profits_max_heap = MaxHeap()
+    capitals_min_heap = MinHeap()
+
+    # adding index of the capital list as well, since the same index corresponds to the profit that capital gives
+    for index, cap in enumerate(capitals):
+        capitals_min_heap.insert((cap, index))
+
+    for _ in range(k):
+
+        while capitals_min_heap and capitals_min_heap.get_min()[0] <= current_capital:
+            capital, index = capitals_min_heap.pop()
+            # populate the profits max heap with the current capital
+            profits_max_heap.insert(profits[index])
+
+        # if there is no profit with the current capital you have.
+        if not profits_max_heap:
+            break
+
+        current_capital += profits_max_heap.get_max()
+
+    return current_capital
+
+
+if __name__ == "__main__":
+    inp = (
+        (0, 1, [1, 1, 2], [1, 2, 3]),
+        (1, 2, [1, 2, 2, 3], [2, 4, 6, 8]),
+        (2, 3, [1, 3, 4, 5, 6], [1, 2, 3, 4, 5]),
+        (1, 3, [1, 2, 3, 4], [1, 3, 5, 7]),
+        (7, 2, [6, 7, 8, 10], [4, 8, 12, 14]),
+        (2, 4, [2, 3, 5, 6, 8, 12], [1, 2, 5, 6, 8, 9])
+    )
+    num = 1
+    for i in inp:
+        print(f"{num}.\tProject capital requirements:  {i[2]}")
+        print(f"\tProject expected profits:      {i[3]}")
+        print(f"\tNumber of projects:            {i[1]}")
+        print(f"\tStart-up capital:              {i[0]}")
+        print("\n\tMaximum capital earned: ",
+              maximum_capital(i[0], i[1], i[2], i[3]))
+        print("-" * 100, "\n")
+        num += 1
--- utils/max_heap.py
+++ utils/max_heap.py
@@ -4,12 +4,12 @@
 """
 Since python only supports min heap, adding this to support max heap to avoid duplication
 """
-from heapq import *
+from heapq import heappush, heappop
 
 
 class MaxHeap:
 
-    def __int__(self):
+    def __init__(self):
         self.max_heap_list = []
 
     def insert(self, value):
@@ -17,6 +17,9 @@ class MaxHeap:
 
     def get_max(self):
         return -self.max_heap_list[0]
+
+    def pop(self):
+        return -heappop(self.max_heap_list)
 
     def __len__(self):
         return len(self.max_heap_list)
@@ -27,3 +30,12 @@ class MaxHeap:
             out += str(i) + ", "
         out = out[:-2] + "]"
         return out
+
+
+if __name__ == "__main__":
+    sample_list = [1, 2, 3, 4, 5]
+    max_heap = MaxHeap()
+    for i in sample_list:
+        max_heap.insert(i)
+
+    print(max_heap.get_max())
