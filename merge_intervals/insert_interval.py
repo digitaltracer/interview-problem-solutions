47bca8cd4aedd0ec2d685fbde5d61072fa7a23d0
Adarsh N B <digitaltracer@users.noreply.github.com>
2023-11-25 12:27:10 +0530

    add insert interval solution

--- merge_intervals/insert_interval.py
+++ merge_intervals/insert_interval.py
@@ -10,22 +10,53 @@ the end time of the interval.
 from typing import List
 
 
-def insert_interval(existing_intervals: List[List[int]], new_interval: List[List[int]]) -> List[List[int]]:
+def insert_interval(existing_intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
     if not existing_intervals:
-        return new_interval
+        return [new_interval]
 
     if not new_interval:
         return existing_intervals
 
-    new_interval_index = 0
     existing_interval_index = 0
     result = []
 
-    while existing_intervals[existing_interval_index][0] < new_interval[0][0]:
+    while existing_intervals[existing_interval_index][0] < new_interval[0]:
         result.append(existing_intervals[existing_interval_index])
         existing_interval_index += 1
 
-    for i in range(existing_interval_index, len(existing_intervals)):
-        pass
+    if not result or result[-1][1] < new_interval[0]:
+        result.append(new_interval)
+
+    else:
+        result[-1][-1] = max(new_interval[-1], result[-1][-1])
+
+    while existing_interval_index < len(existing_intervals):
+        if result[-1][-1] > existing_intervals[existing_interval_index][0]:
+            result[-1][-1] = max(result[-1][-1], existing_intervals[existing_interval_index][1])
+        else:
+            result.append(existing_intervals[existing_interval_index])
+        existing_interval_index += 1
 
     return result
+
+
+def main():
+    new_interval = [[5, 7], [8, 9], [10, 12], [1, 3], [1, 10]]
+    existing_intervals = [
+        [[1, 2], [3, 5], [6, 8]],
+        [[1, 3], [5, 7], [10, 12]],
+        [[8, 10], [12, 15]],
+        [[5, 7], [8, 9]],
+        [[3, 5]]
+    ]
+
+    for i in range(len(new_interval)):
+        print(i + 1, ".\tExiting intervals: ", existing_intervals[i], sep="")
+        print("\tNew interval: ", new_interval[i], sep="")
+        output = insert_interval(existing_intervals[i], new_interval[i])
+        print("\tUpdated intervals: ", output, sep="")
+        print("-" * 100)
+
+
+if __name__ == "__main__":
+    main()
